a0d93a5385f4b43fc26ff0cef8d7487aaf4f476f
Lokeswara Reddy <lokeswar8192@gmail.com>
2026-09-15 11:06:20 +0530

    rhffgbnh

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,29 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+
+// Global styles
 import "./styles/globals.css";
 import "./styles/layout.css";
 import "./styles/home.css";
+import "./styles/products.css";
+import "./styles/product-details.css";
+import "./styles/cart.css";
+import "./styles/address.css";
+import "./styles/orders.css";
+import "./styles/order-details.css";
+import "./styles/admin.css";
+import "./styles/admin-products.css";
+import "./styles/admin-categories.css";
+import "./styles/admin-orders.css";
+import "./styles/auth.css";
 
-createRoot(
-    document.getElementById("root"),
-).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
